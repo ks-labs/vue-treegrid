a4db2d4f91e312870815b626cdf4fb0da7e8f01d
Victor Camara Lessa <33801047+victorLessa@users.noreply.github.com>
2022-12-26 16:49:06 -0300

    feat: primeira versão do componente treegrid

---
+++ src/vue-treegrid.vue
@@ -0,0 +1,53 @@
+<template>
+  <table class="table table-bordered tree">
+    <thead>
+      <th v-for="(item, index) in columns" :key="index">
+        {{ item.name }}
+      </th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(item, index) in rows"
+        :key="index"
+        :class="`treegrid-${item.id} ${
+          item.parent_id ? `treegrid-parent-${item.parent_id}` : ''
+        }`"
+      >
+        <td v-for="(column, index) in columns" :key="index">
+          <slot :name="column.field" v-bind:[column.field]="item.name">{{
+            item.name
+          }}</slot>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import "./assets/js/jquery.treegrid";
+export default {
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+    },
+    rows: {
+      type: Array,
+      required: true,
+    },
+    options: {
+      type: Object,
+      default: {},
+    },
+  },
+  created() {
+    $(document).ready(function () {
+      $(".tree").treegrid(this.options);
+    });
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/jquery.treegrid.css";
+</style>
