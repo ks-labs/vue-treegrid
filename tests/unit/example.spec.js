a072f462af361ff4d7af70cdb30db84f694f2bc0
Victor Camara Lessa <33801047+victorLessa@users.noreply.github.com>
2023-03-21 16:30:45 -0300

    chore: alteração teste

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,7 +2,7 @@ describe("HelloWorld.vue", () => {
   it("Testar evento de click row", async () => {
     const page = await globalThis.__BROWSER_GLOBAL__.newPage();
 
-    await page.goto("http://localhost:8080/");
+    await page.goto("http://localhost:4444/");
 
     await page.waitForSelector("#tree tr");
   }, 9999999);
