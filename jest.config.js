821f66e4c23965cc319d9bb72b1b00f97f7bfff5
Victor Camara Lessa <33801047+victorLessa@users.noreply.github.com>
2023-03-21 15:26:46 -0300

    feat: implementar bootstrap table com treegrid

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  preset: "jest-puppeteer",
   testEnvironmentOptions: {
     customExportConditions: ["node", "node-addons"],
   },
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
+  globalSetup: "./tests/setup.js",
+  globalTeardown: "./tests/teardown.js",
+  testEnvironment: "./tests/puppeteer_env.js",
   setupFiles: ["./source/setup-jest.js"],
 };
