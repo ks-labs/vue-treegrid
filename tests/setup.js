47fedd40e49e271d3ae7b4f31b3d80d719e2ac31
Victor Camara Lessa <33801047+victorLessa@users.noreply.github.com>
2023-03-21 16:26:58 -0300

    chore: alteração testes

--- tests/setup.js
+++ tests/setup.js
@@ -6,7 +6,7 @@ const puppeteer = require("puppeteer");
 const DIR = path.join(os.tmpdir(), "jest_puppeteer_global_setup");
 
 module.exports = async function () {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   // store the browser instance so we can teardown it later
   // this global is only available in the teardown but not in TestEnvironments
   globalThis.__BROWSER_GLOBAL__ = browser;
